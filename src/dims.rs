3e84fd8b4ccf3f3828784ad821ec4cd2c75a0ba8
Charles Turner <charles.turner@anu.edu.au>
2024-12-31 13:34:51 +0800

    Cleanup, making progress

---
+++ src/dims.rs
@@ -0,0 +1,42 @@
+use colored::Colorize;
+
+pub fn get_dim_info(file: &netcdf::File, dim_name : &String ) {
+    // Take references to a file and a dimension in it, and print their size.
+
+    let dim_opt = file.dimensions().find(|dim| dim.name() == *dim_name);
+
+    // Nasty - get rid of unwrap if possible.
+    let dim = dim_opt.unwrap();
+
+    let is_dim_unlimited = dim.is_unlimited();
+    let dim_len = dim.len();
+
+    let dim_info = DimInfo::new(dim_name.clone(), is_dim_unlimited, dim_len);
+
+    let dim_fstr = format_dim_info(dim_info);
+
+    println!("{}",dim_fstr)
+
+}
+
+struct DimInfo {
+    name : String,
+    is_unlimited : bool,
+    len : usize,
+}
+
+impl DimInfo {
+    fn new(name : String, is_unlimited : bool, len : usize) -> Self{
+        DimInfo{name, is_unlimited, len}
+    }
+}
+
+fn format_dim_info(dim_info : DimInfo) -> String{
+    // Return info about a dimension
+    if dim_info.is_unlimited {
+        format!("\t{} = {} ; // ({} currently) ", dim_info.name.blue().bold(), "UNLIMITED".yellow().bold(),  dim_info.len)
+    } else {
+        format!("\t{} = {} ;", dim_info.name.blue().bold(), dim_info.len)
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+mod dims;
+
 use netcdf::{self};
+use std::path::Path;
+use colored::Colorize;
+use dims::get_dim_info;
 
 trait HasName {
     fn name(&self) -> &str;
@@ -10,23 +15,7 @@ impl<'a> HasName for netcdf::Attribute<'a> {
     }
 }
 
-struct DimWrapper<'a> {
-    _dim: netcdf::Dimension<'a>,
-    name : String,
-}
 
-impl<'a> DimWrapper<'a> {
-    fn new(_dim: netcdf::Dimension<'a>) -> Self {
-        let name = _dim.name().clone();
-        DimWrapper { _dim, name}
-    }
-}
-
-impl<'a> HasName for DimWrapper<'a> {
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
 
 struct VarWrapper<'a> {
     _var : netcdf::Variable<'a>,
@@ -51,28 +40,59 @@ impl<'a> HasName for VarWrapper<'a> {
 
 
 fn main() -> Result<(), netcdf::Error> {
-    let file = netcdf::open(
-        "/Users/u1166368/Rust/nchdr/tests/data/access-om2/output001/ocean/ocean.nc"
-    )?;
+
+    let path = Path::new("/Users/u1166368/Rust/nchdr/tests/data/access-om2/output001/ocean/ocean.nc");
+
+    let file: netcdf::File = netcdf::open(path)?;
+
+    let file_stem = extract_fname(&path)?;
+    print_skeleton_opening(&file_stem);
+
     let variables: Vec<_> = file.variables().collect();
     let dimensions: Vec<_> = file.dimensions().collect();
     let global_attrs: Vec<_> = file.attributes().collect();
 
+    println!("{}","dimensions:".green());
+    for dim in dimensions {
+        get_dim_info(&file, &dim.name());
+    }
+
     let wrapped_vars = variables.into_iter().map(VarWrapper::new).collect();
+    println!("{}","variables:".green());
     print_attrs(wrapped_vars);
 
-    let wrapped_dims = dimensions.into_iter().map(DimWrapper::new).collect();
-    print_attrs(wrapped_dims);
 
+    println!("{}","// global attributes:".green());
     print_attrs(global_attrs);
+    print_skeleton_close();
     
     Ok(())
 }
 
 
 fn print_attrs<T>(attrs : Vec<T>) where T: HasName {
-    println!("\n");
     for attr in attrs {
-        println!("Attributes: {}", attr.name())
+        println!("\t{} ;",  attr.name())
     }
+}
+
+fn print_skeleton_opening( f_stem : &str){
+    println!("netcdf {} {{ ", f_stem.cyan().bold());
+}
+
+fn print_skeleton_close(){
+    println!("}}\n")
+}
+
+fn extract_fname<'a>(f_handle: &'a std::path::Path) -> Result<&str, &str>{
+    // Take the file handle, get the file stem, extract it.
+
+    match f_handle.file_stem() {
+        Some(stem) => match stem.to_str() {
+            Some(stem_str) => Ok(stem_str),
+            None => Err("Could not convert file stem to valid utf-8 string"),
+        }
+        None => Err("Could not identify filename"),
+    }
+
 }
